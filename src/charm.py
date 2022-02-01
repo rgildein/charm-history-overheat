2207ee8f2405f39c57a90693f6f9e932e58f41ae
Robert Gildein <robert.gildein@canonical.com>
2022-02-01 20:36:36 +0100

    test

--- src/charm.py
+++ src/charm.py
@@ -15,7 +15,6 @@ develop a new k8s charm using the Operator Framework:
 import logging
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus
 
@@ -25,31 +24,17 @@ logger = logging.getLogger(__name__)
 class CharmHistoryOverheatCharm(CharmBase):
     """Charm the service."""
 
-    _config_history = StoredState()
-
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self._config_history.set_default(test_key=[])
+        if self.model.config["history"]:
+            self.framework.tacked(["test-key"])
+
         self.unit.status = ActiveStatus("unit is ready")
-
-    def tack(self) -> None:
-        """Tracking a specific key in config."""
-        current_value = self.config.get("test-key")
-        try:
-            last_value = self._config_history.test_key[-1]
-        except IndexError:
-            last_value = None
-
-        if last_value != current_value:
-            self._config_history.test_key.append(current_value)
 
     def _on_config_changed(self, _):
         """Testing config changed handler."""
-        if self.config["history"]:
-            logger.info("tracking config history is enabled")
-            self.tack()
-            logger.info(f"tracking is done")
+        logger.info("config-changed: test-key={}".format(self.config["test-key"]))
 
 
 if __name__ == "__main__":
